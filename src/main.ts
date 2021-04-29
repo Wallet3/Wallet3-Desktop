67b30bee9bf19279fbebe195d5164edff4cbb6be
UnsignedInt8 <unsignedint8@gmail.com>
2021-04-29 19:11:00 +0800

    [Backend] Add IPC Security flowchart

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
+import './backend/App';
+
 import { BrowserWindow, app } from 'electron';
-
-import App from './backend/App';
-
-App;
 
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
