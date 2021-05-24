52e682f4f70a49b8869dd8c37c4d9d145be23b15
UnsignedInt8 <unsignedint8@gmail.com>
2021-05-24 10:54:22 +0800

    [UI] Fix MKR non-standard ABI

--- src/ui/viewmodels/AddTokenVM.ts
+++ src/ui/viewmodels/AddTokenVM.ts
@@ -3,8 +3,17 @@ import { makeAutoObservable, runInAction } from 'mobx';
 
 import { AccountVM } from './AccountVM';
 import ERC20ABI from '../../abis/ERC20.json';
+import MKRABI from '../../abis/MKR.json';
 import NetworksVM from './NetworksVM';
 import { UserToken } from './models/UserToken';
+
+function hex2str(hex: string) {
+  var hex = hex.toString(); //force conversion
+  var str = '';
+  for (var i = 0; i < hex.length && hex.substr(i, 2) !== '00'; i += 2)
+    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  return str;
+}
 
 export class AddTokenVM {
   loading = false;
@@ -34,14 +43,21 @@ export class AddTokenVM {
     this.symbol = '';
     this.tokenAddress = value;
 
+    const isMKR = value.toLowerCase() === '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2';
+
     try {
-      const erc20 = new ethers.Contract(value, ERC20ABI, NetworksVM.currentProvider);
-      const [balance, name, decimals, symbol] = await Promise.all([
+      const erc20 = new ethers.Contract(value, isMKR ? MKRABI : ERC20ABI, NetworksVM.currentProvider);
+      let [balance, name, decimals, symbol] = await Promise.all([
         erc20.balanceOf(this.accountVM.address),
         erc20.name(),
         erc20.decimals(),
         erc20.symbol(),
       ]);
+
+      if (isMKR) {
+        name = hex2str(name.substring(2));
+        symbol = hex2str(symbol.substring(2));
+      }
 
       runInAction(() => {
         this.balance = ethers.utils.formatUnits(balance, decimals);
@@ -51,6 +67,7 @@ export class AddTokenVM {
         this.isValid = true;
       });
     } catch (error) {
+      console.log(error);
       runInAction(() => (this.isValid = false));
     } finally {
       runInAction(() => (this.loading = false));
