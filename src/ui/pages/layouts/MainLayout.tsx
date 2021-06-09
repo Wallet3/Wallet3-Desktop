e4b14c3939a14cf7447c24d0c1e1f9fba3194022
UnsignedInt8 <unsignedint8@gmail.com>
2021-06-09 16:13:45 +0800

    [UI] Fix main layout active tab

--- src/ui/pages/layouts/MainLayout.tsx
+++ src/ui/pages/layouts/MainLayout.tsx
@@ -1,7 +1,6 @@
 import './MainLayout.css';
 
-import { Link, useRouteMatch } from 'react-router-dom';
-import React, { useEffect, useState } from 'react';
+import { Link, useLocation, useRouteMatch } from 'react-router-dom';
 import { Route, Switch } from 'react-router';
 import { Settings, Wallet } from '../app';
 
@@ -10,9 +9,9 @@ import { CurrencyVM } from '../../viewmodels/settings/CurrencyVM';
 import Feather from 'feather-icons-react';
 import { LangsVM } from '../../viewmodels/settings/LangsVM';
 import { NetworksVM } from '../../viewmodels/NetworksVM';
+import React from 'react';
 import { SkeletonTheme } from 'react-loading-skeleton';
 import { WalletVM } from '../../viewmodels/WalletVM';
-import { makeAutoObservable } from 'mobx';
 import { observer } from 'mobx-react-lite';
 
 interface Props {
@@ -23,22 +22,12 @@ interface Props {
   langsVM: LangsVM;
 }
 
-class LayoutStatus {
-  activeTab = 0;
-
-  constructor() {
-    makeAutoObservable(this);
-  }
-
-  setActiveTab(index: number) {
-    this.activeTab = index;
-  }
-}
-
-const layoutStatus = new LayoutStatus();
-
 export default observer((args: Props) => {
   const { path, url } = useRouteMatch();
+  const { pathname } = useLocation();
+
+  let activeTab = 0;
+  if (pathname.endsWith('settings')) activeTab = 1;
 
   return (
     <SkeletonTheme color="#eeeeee90" highlightColor="#f5f5f5d0">
@@ -56,15 +45,15 @@ export default observer((args: Props) => {
         </div>
 
         <div className="tabs">
-          <Link to={`${url}`} onClick={() => layoutStatus.setActiveTab(0)}>
-            <div className={layoutStatus.activeTab === 0 ? 'active' : ''}>
+          <Link to={`${url}`}>
+            <div className={activeTab === 0 ? 'active' : ''}>
               <Feather icon="credit-card" size={20} />
               <span>Wallet</span>
             </div>
           </Link>
 
-          <Link to={`${url}/settings`} onClick={() => layoutStatus.setActiveTab(1)}>
-            <div className={layoutStatus.activeTab === 1 ? 'active' : ''}>
+          <Link to={`${url}/settings`}>
+            <div className={activeTab === 1 ? 'active' : ''}>
               <Feather icon="settings" size={20} />
               <span>Settings</span>
             </div>
