770f261954e02925e14be7bc757e3cf27cbc9a62
UnsignedInt8 <unsignedint8@gmail.com>
2021-06-21 16:27:03 +0800

    [UI] Frameless window

--- src/main.ts
+++ src/main.ts
@@ -113,6 +113,7 @@ const createWindow = async (): Promise<void> => {
     minWidth: 360,
     minHeight: 540,
     // frame: process.platform === 'win32' ? true : false, // https://github.com/electron/electron/issues/20754
+    frame: false,
     titleBarStyle: isMac ? 'hiddenInset' : 'hidden',
     acceptFirstMouse: true,
     webPreferences: {
