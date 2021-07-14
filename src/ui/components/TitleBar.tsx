f7c77cc0198b0b7cc4a73a9be53944abc71c97ba
UnsignedInt8 <unsignedint8@gmail.com>
2021-07-14 15:52:58 +0800

    [UI] Fix title bar buttons

--- src/ui/components/TitleBar.tsx
+++ src/ui/components/TitleBar.tsx
@@ -14,14 +14,14 @@ interface Props {
 export default ({ onClose, onMinimize, onMaximize }: Props) => {
   return (
     <div className="system-title-bar">
-      <button className="close" onClick={onClose}>
-        <img src={close} draggable={false} />
-      </button>
       <button className="minimize" onClick={onMinimize}>
         <img src={minimize} draggable={false} />
       </button>
       <button className="maximize" onClick={onMaximize}>
         <img src={maximize} draggable={false} />
+      </button>
+      <button className="close" onClick={onClose}>
+        <img src={close} draggable={false} />
       </button>
     </div>
   );
