e188d7d84ab235d77c19c1980aee4b5559391790
UnsignedInt8 <unsignedint8@gmail.com>
2021-08-17 14:30:18 +0800

    [UI] Save customized RPC URL, update i18n

--- src/ui/pages/app/CustomizeNetwork.tsx
+++ src/ui/pages/app/CustomizeNetwork.tsx
@@ -1,12 +1,13 @@
 import './CustomizeNetwork.css';
 
+import React, { useRef } from 'react';
+import { getChainProviderMaskUrl, saveCustomizedRPC } from '../../../common/Provider';
+
 import { Application } from '../../viewmodels/Application';
 import Feather from 'feather-icons-react';
 import { NavBar } from '../../components';
 import { NetworkIcons } from '../../misc/Icons';
 import { Networks } from '../../../common/Networks';
-import React from 'react';
-import { getChainProviderMaskUrl } from '../../../common/Provider';
 import { getExplorerUrl } from '../../../misc/Url';
 import { useRouteMatch } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
@@ -14,9 +15,16 @@ import { useTranslation } from 'react-i18next';
 export default ({ app }: { app: Application }) => {
   const { t } = useTranslation();
   const { chainId } = useRouteMatch().params as { chainId?: string };
-  console.log(useRouteMatch());
 
   const network = Networks.find((n) => `${n.chainId}` === chainId);
+
+  const rpcRef = useRef<HTMLInputElement>();
+  const explorerRef = useRef<HTMLInputElement>();
+
+  const save = () => {
+    saveCustomizedRPC(network.chainId, rpcRef.current.value, explorerRef.current.value);
+    app.history.goBack();
+  };
 
   return (
     <div className="page customize-network">
@@ -36,19 +44,21 @@ export default ({ app }: { app: Application }) => {
         <div className="app-item">
           <span>RPC URL:</span>
           <div>
-            <input type="text" defaultValue={getChainProviderMaskUrl(network.chainId)} /> <Feather icon="edit-3" size={12} />
+            <input ref={rpcRef} type="text" defaultValue={getChainProviderMaskUrl(network.chainId)} />{' '}
+            <Feather icon="edit-3" size={12} />
           </div>
         </div>
 
         <div className="app-item">
           <span>{t('Block Explorer')}:</span>
           <div>
-            <input type="text" defaultValue={getExplorerUrl(network.chainId)} /> <Feather icon="edit-3" size={12} />
+            <input ref={explorerRef} type="text" defaultValue={getExplorerUrl(network.chainId)} />{' '}
+            <Feather icon="edit-3" size={12} />
           </div>
         </div>
       </div>
 
-      <button>{t('Save')}</button>
+      <button onClick={(_) => save()}>{t('Save')}</button>
     </div>
   );
 };
